ab309fa34256627eda7091e5366591ade11c3177
SerenityZenDev <tjrqo2000@gmail.com>
2025-02-04 11:09:38 +0900

    데이터 표시

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,25 @@ function AboutPage(){
   )
 }
 
-export default function MyApp(){
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+export default function Profile() {
   return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <img className="avatar" src="#" alt="Avatar"/>
-      <AboutPage />
-    </div>
-  )
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
 }
