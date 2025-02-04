969c38fd7ae8a3f5acda3cdc2dc26620820983ff
SerenityZenDev <tjrqo2000@gmail.com>
2025-02-04 11:55:39 +0900

    이벤트에 대응하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 function MyButton(){
+  function handleClick(){
+    alert('You clicked me!');
+  }
+
   return (
-    <button>
-      I'm a button
+    <button onClick={handleClick}>
+      Click me
     </button>
+  )
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
   );
 }
 
@@ -15,25 +28,25 @@ function AboutPage(){
   )
 }
 
-const user = {
-  name: 'Hedy Lamarr',
-  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-  imageSize: 90,
-};
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
 
-export default function Profile() {
-  return (
-    <>
-      <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={'Photo of ' + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize
-        }}
-      />
-    </>
-  );
-}
+// export default function ShoppingList() {
+//   const listItems = products.map(product =>
+//     <li
+//       key={product.id}
+//       style={{
+//         color: product.isFruit ? 'magenta' : 'darkgreen'
+//       }}
+//     >
+//       {product.title}
+//     </li>
+//   );
+
+//   return (
+//     <ul>{listItems}</ul>
+//   );
+// }
